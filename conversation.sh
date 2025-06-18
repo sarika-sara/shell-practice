25cc368025c1edb72a854fb31b1e3790c8e4773c
sarika-cloud <sarikakan6@gmail.com>
2025-06-17 23:03:22 -0400

    updated

--- conversation.sh
+++ conversation.sh
@@ -1,8 +1,13 @@
-echo "modi:hi there, how are you?
-echo "sarika:hi modi, I am fine, how are you?
-echo "modi: I am good, thanks for asking!" 
-echo "sarika: What are you doing today?"
-echo "modi: I am going to the market, do you want to join?"
-echo "sarika: Sure, I would love to join you!"
-echo "modi: Great! Let's meet at 5 PM."
-echo "sarika: Sounds good! See you then!"
+#!/bin/bash
+Person1=$modi
+Person2=$Sarika
+
+echo "$Person1=hi there, how are you?"
+echo "$Person2= Hello $Person1, I am fine, how are you?"
+echo "$Person1: I am doing well, thank you!"                
+echo "$Person1: I am good, thanks for asking!" 
+echo "$Person2: What are you doing today?"
+echo "$Person1: I am going to the market, do you want to join?"
+echo "$Person2: Sure, I would love to join you!"
+echo "$Person1: Great! Let's meet at 5 PM."
+echo "$Person2: Sounds good! See you then!"

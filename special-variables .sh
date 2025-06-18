3add81d06a18c655b530730059a53bd5a1ef069e
sarika-cloud <sarikakan6@gmail.com>
2025-06-17 23:17:15 -0400

    updated

---
+++ special-variables .sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Variables passed = $@"
+echo "No.of Variables passed = $#"
+echo "Script Name is = $0"
+echo "Present working directory is = $PWD"
+echo "Present user is = $USER"
